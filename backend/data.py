79b929b056fd55614a16033d247921671770d061
Makeba Waddy <153320339+makebashangwe@users.noreply.github.com>
2026-07-09 18:29:01 -0400

    updated error in data.py "activity_rule"

--- backend/data.py
+++ backend/data.py
@@ -1,5 +1,5 @@
 users = [] #fake user DB
-activity_rule = [] #fake activity rules DB
+activity_rules = [] #fake activity rules DB
 activity_sessions = [] #fake session DB
 goals = [] #fake goals DB (atp we know its fake but imma keep it going lol)
 rewards = []
